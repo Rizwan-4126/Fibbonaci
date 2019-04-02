3cc65cdcffc5e59f49130165cc3f198dc793f216
Rizwan-4126 <48685450+Rizwan-4126@users.noreply.github.com>
2019-04-02 09:12:57 +0700

    Add files via upload

---
+++ untitled1/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n;
+
+        System.out.print("Masukan Jumlah Deret : ");
+        n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int x = cari(i);
+            System.out.print(x + " ");
+        }
+
+    }
+
+    public static int cari(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return cari(n - 1) + cari(n - 2);
+        }
+    }
+}
